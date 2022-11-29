fd80415d9c7629430a10192c31ff6a4589bf7eb6
katescha8 <kate.schapo.5@gmail.com>
2022-11-29 22:07:31 +0300

    Добавьте файлы проекта.

---
+++ Classwork11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using ClassWork11;
+
+
+namespace ClassWork11
+{
+    class Program
+    {
+        public class Bank
+        {
+            public BankAccount CreateAccount()
+            {
+                BankAccount opened = new BankAccount();
+                accounts[opened.Number()] = opened;
+                return opened;
+            }
+            public void DeleteAccount(long number)
+            {
+                accounts.Remove(accounts[number]);
+            }
+            private Hashtable accounts = new Hashtable();
+        }
+        public class BankAccount
+        {
+            private long number;
+            internal BankAccount() { Generic(); }
+            public void Generic()
+            {
+                Random r = new Random();
+                number = r.NextInt64(1000000000000000, 10000000000000000);
+            }
+            public long Number() { return number; }
+        }
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            BankAccount bankacc1 = bank.CreateAccount();
+            BankAccount bankacc2 = bank.CreateAccount();
+            bank.DeleteAccount(bankacc1.Number());
+            Console.WriteLine(bankacc1.Number());
+        }
+    }
+}
---
+++ Classwork12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+
+namespace ClassWork12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
